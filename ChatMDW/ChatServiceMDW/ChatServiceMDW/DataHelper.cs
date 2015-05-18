a9d707961b85734fbb2d938b6e054c33fbe1d470
IvanaRaykova <ivraikovaa13@gmail.com>
2015-05-18 21:55:46 +0300

    ChatMDW
    global chat for the application source code

---
+++ ChatMDW/ChatServiceMDW/ChatServiceMDW/DataHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+
+
+namespace ChatServiceMDW
+{
+    class DataHelper
+    {
+        private OleDbConnection connection;
+
+        //username as a parameter in the constructor
+        public DataHelper()
+        {
+            //this is for access to an ACCESS databse
+            String info = "Provider=Microsoft.ACE.OLEDB.12.0";
+
+            String databaseInfo = "DataSource";
+
+            String connectionInfo = info + ";" + databaseInfo;
+            connection = new OleDbConnection();
+
+        }
+        public Player isUser(string username, string password)
+        {
+            Player player = null;
+
+            String sql = "Select statement";
+            OleDbCommand command = new OleDbCommand(sql,connection);
+            try
+            {
+                //connection.Open;
+                OleDbDataReader reader = command.ExecuteReader();
+
+                reader.Read();
+
+
+            }
+            catch
+            {
+
+
+            }
+            finally
+            {
+                //conection.Close();
+            }
+            return player;
+        }
+
+    }
+}
---
+++ ChatMDW/ChatServiceMDW/ChatServiceMDW/ILogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ChatServiceMDW;
+
+namespace ChatServiceMDW
+{
+    [ServiceContract(Namespace = "LoginContract")]
+    public interface ILogin
+    {
+        [OperationContract]
+        bool Login(string username, string password);
+    }
+}
